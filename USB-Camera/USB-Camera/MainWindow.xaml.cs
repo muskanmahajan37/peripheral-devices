785ebcbcafef5051ed66f58b3bfd2a551704fd92
hulewiczkamil <hulewicz.k@gmail.com>
2020-10-20 17:58:27 +0200

    Why doesn't it work the way it should

--- USB-Camera/USB-Camera/MainWindow.xaml.cs
+++ USB-Camera/USB-Camera/MainWindow.xaml.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -27,66 +28,100 @@ namespace USB_Camera
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        public ObservableCollection<FilterInfo> VideoDevices { get; set;}
+
+        public FilterInfo CurrentDevice
+        {
+            set { _currentDevice = value; this.OnPropertyChanged("CurrentDevice"); }
+            get { return _currentDevice; }
+        }
+        private FilterInfo _currentDevice;
+
+        private IVideoSource _videoSource;
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
-            LoadAvailableDevices();
+            GetVideoDevices();
+            this.Closing += MainWindow_Closing;
+        }
+        
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            StopCamera();
+        }
 
+        private void StopCamera()
+        {
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                _videoSource.SignalToStop();
+                _videoSource.NewFrame -= new NewFrameEventHandler(video_NewFeame);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string name)
+        protected void OnPropertyChanged(string propertyName)
         {
-            PropertyChangedEventHandler propertyChangedHandler = this.PropertyChanged;
-            if(propertyChangedHandler != null)
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if(handler != null)
             {
-                var propertyEvent = new PropertyChangedEventArgs(name);
-                propertyChangedHandler(this, propertyEvent);
-            }
-        }
-        
-        private FilterInfo selectedDev;
-
-        public FilterInfo selectedDevice
-        {
-            set { selectedDev = value; this.OnPropertyChanged("SelectedCamera"); }
-            get { return selectedDev; }
-        }
-        private void LoadAvailableDevices()
-        {
-            SelectableDevices = new ObservableCollection<FilterInfo>();
-            foreach (FilterInfo i in new FilterInfoCollection(FilterCategory.VideoInputDevice)) SelectableDevices.Add(i);
-            if (SelectableDevices.Any()) MessageBox.Show("Urządzenie odnaleziono", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
-        
-        private IVideoSource videoSource;
-        
-        public ObservableCollection<FilterInfo> SelectableDevices
-        {
-            set;
-            get;
-        }
-
-        private void capture(object o, AForge.Video.NewFrameEventArgs arguments)
-        {
-
-        }
-
-        private void GetCameraSource()
-        {
-            if (selectedDevice != null)
-            {
-                videoSource = new VideoCaptureDevice(selectedDevice.MonikerString);
-                videoSource.NewFrame += null;
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
             }
         }
 
-        private void ComboBoxCameraSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
+        private void GetVideoDevices()
+        {
+            VideoDevices = new ObservableCollection<FilterInfo>();
+            foreach (FilterInfo i in new FilterInfoCollection(FilterCategory.VideoInputDevice))
+            {
+                VideoDevices.Add(i);
+            }
+            if (VideoDevices.Any())
+            {
+                CurrentDevice = VideoDevices[0];
+            }
+        }
+        
+        
+
+        private void video_NewFeame(object sender, AForge.Video.NewFrameEventArgs arguments)
+        {
+            try
+            {
+                BitmapImage bitmapImage;
+                using (var bitmap = (Bitmap)arguments.Frame.Clone())
+                {
+                    bitmapImage = bitmap.ToBitmapImage();
+                }
+                bitmapImage.Freeze();
+                Dispatcher.BeginInvoke(new ThreadStart(delegate { VideoPlayer.Source = bitmapImage; }));
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error:\n", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                StopCamera();
+            }
         }
 
+        
+
+        private void startCamera()
+        {
+            if (CurrentDevice != null)
+            {
+                MessageBox.Show("XD");
+                _videoSource = new VideoCaptureDevice(CurrentDevice.MonikerString);
+                _videoSource.NewFrame += null;
+                _videoSource.Start();
+            }
+        }
+
+       
         private void SaturationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
@@ -96,12 +131,7 @@ namespace USB_Camera
         {
 
         }
-
-        private void BtnStart_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        
         private void BtnStop_Click(object sender, RoutedEventArgs e)
         {
 
@@ -120,6 +150,12 @@ namespace USB_Camera
         private void BtnScreenshot_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void BtnStart_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hahahihi");
+            startCamera();
         }
     }
 }
