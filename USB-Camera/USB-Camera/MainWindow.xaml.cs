1ecc00b6b3b720e30a67123a6c08dd7b6f214813
hulewiczkamil <hulewicz.k@gmail.com>
2020-10-20 12:32:42 +0200

    Interface done, AForge imported

--- USB-Camera/USB-Camera/MainWindow.xaml.cs
+++ USB-Camera/USB-Camera/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using AForge.Video;
+using AForge.Video.DirectShow;
+
 namespace USB_Camera
 {
     /// <summary>
@@ -23,6 +26,46 @@ namespace USB_Camera
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ComboBoxCameraSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SaturationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void ContrastSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRecord_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnStopRecording_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnScreenshot_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
